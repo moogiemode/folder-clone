f10dd7ff84f968f451eab9f8890b3c407838cc73
Edwin Ekpenyong <moogiemode@gmail.com>
2025-01-14 00:22:06 -0800

    Updated: src/App.css src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import { FolderNameInput } from './FolderNameInput';
 import { CloneButton } from './CloneButton';
 import { FileExtensionsInput } from './FileExtensionsInput';
