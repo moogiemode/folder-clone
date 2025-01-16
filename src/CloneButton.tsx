40a4fa5dac340d6bba341cf6b64128a14cc213de
Edwin Ekpenyong <moogiemode@gmail.com>
2025-01-15 22:05:07 -0800

    counter fixed

--- src/CloneButton.tsx
+++ src/CloneButton.tsx
@@ -22,9 +22,9 @@ export const CloneButton: FC<CloneButtonProps> = ({ sourceFolderPath, outputFold
       if (files.filter(file => file.name !== '.DS_Store').length > 0) throw new Error('Output folder is not empty');
     });
 
-    (document.getElementById('my_modal_1') as HTMLDialogElement).showModal();
+    (document.getElementById('file-clone-modal') as HTMLDialogElement).showModal();
     try {
-      await cloneDir({ sourceFolderPath, outputFolderPath, extToClone: new Set(fileExtensions) });
+      await cloneDir({ sourceFolderPath, outputFolderPath, extToClone: new Set(fileExtensions), onCloneCallback: () => setNumCloned(prev => prev + 1) });
     } catch (error) {
       setError((error as Error).message);
     }
@@ -36,13 +36,13 @@ export const CloneButton: FC<CloneButtonProps> = ({ sourceFolderPath, outputFold
       <button className="btn btn-accent" onClick={handleClone}>
         Clone
       </button>
-      <dialog id="my_modal_1" className="modal">
+      <dialog id="file-clone-modal" className="modal">
         <div className="modal-box flex flex-col gap-12 justify-center items-center p-8">
           <h3 className="font-bold text-lg">{cloning ? 'Cloning Files...' : 'Files Cloned'}</h3>
           <div className="text-center p-4 w-full flex-1 overflow-auto font-bold">Files Cloned: {numCloned}</div>
           {cloning ? (
             <div className="flex flex-col gap-4 p-4 max-w-full items-center justify-center flex-1 max-h-full overflow-hidden">
-              <span className="loading loading-ring loading-lg"></span>
+              <span className="loading loading-ring loading-lg"/>
               {error && <div className="text-center p-4 w-full flex-1 overflow-auto text-error">{error}</div>}
             </div>
           ) : (
@@ -66,7 +66,7 @@ const cloneDir = async ({ sourceFolderPath, outputFolderPath, extToClone, onClon
       return;
     } else if (file.isDirectory) {
       await mkdir(await join(outputFolderPath, file.name));
-      await cloneDir({ sourceFolderPath: await join(sourceFolderPath, file.name), outputFolderPath: await join(outputFolderPath, file.name), extToClone });
+      await cloneDir({ sourceFolderPath: await join(sourceFolderPath, file.name), outputFolderPath: await join(outputFolderPath, file.name), extToClone, onCloneCallback });
     } else {
       if (!extToClone || extToClone?.has(file.name.split('.').pop() || '')) {
         const sourceFile = await join(sourceFolderPath, file.name);
